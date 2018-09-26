5b309e29ad6e0df80e88d260d297cde48e37d654
k0oAcevn <k0olano0.ltd@gmail.com>
2018-09-26 13:21:30 +0700

    No message

--- project-name/src/app/app.module.ts
+++ project-name/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatTableModule } from '@angular/material/table'; 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -19,7 +18,7 @@ import { AppRoutingModule } from './/app-routing.module';
     AppComponent,HomeComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, FormsModule, HttpModule, BrowserAnimationsModule, NoopAnimationsModule, MatButtonModule, MatCheckboxModule
+    BrowserModule, AppRoutingModule, FormsModule, HttpModule, BrowserAnimationsModule, MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent],
